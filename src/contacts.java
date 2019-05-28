cfc5e0dc86bc27280680459da05338ec45ee4ddb
Derek Noble <dereknoble@Dereks-MacBook-Pro.local>
2019-05-28 14:12:16 -0500

    1 and 2 complete

--- src/contacts.java
+++ src/contacts.java
@@ -6,7 +6,7 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.Scanner;
 public class contacts {
 
 
@@ -24,9 +24,28 @@ public class contacts {
             e.printStackTrace();
         }
     }
+    public static void add(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the name that you would like add.");
+        String newName = sc.nextLine();
+        System.out.println("Please enter their phone number number");
+        String newNumber = sc.nextLine();
+        String newContact = newName + " | " + newNumber;
+        String directory = "data";
+        List<String> updatedList = new ArrayList<>();
+        Path folder = Paths.get(directory);
+        Path file = Paths.get(directory, "contacts.txt");
+        try {
+            Files.write(file, Arrays.asList(newContact), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
+        add();
         read();
+
 
 
 
